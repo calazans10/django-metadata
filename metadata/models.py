dfe2ae19b7ec04efb0578bcc676ef355a7ceba75
Jeferson Farias Calazans <calazans10@gmail.com>
2014-01-09 15:52:42 -0200

    Update models.py

--- metadata/models.py
+++ metadata/models.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import json
 from django.db import models
-from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
 
 
 class MetaDataManager(models.Manager):
